ab18e1efcfd6bf073fb0c4219a3f872f3dc236cd
techtake <27718637+techtake@users.noreply.github.com>
2020-11-04 09:31:51 +0900

    display の表示を修正(本番環境で要確認)-#42

--- controller/watcher/src/watch.py
+++ controller/watcher/src/watch.py
@@ -42,7 +42,7 @@ def get_contract_address():
     except Exception as e:
         print('cannot load contract instance')
         print(e)
-    
+
     return w3, contract, contract_address
 
 
@@ -68,31 +68,32 @@ def handle_event(w3, contract, event):
     if locked:
         img = Image.open('image/display_locked_qr.jpeg')
         img = ImageTk.PhotoImage(img)
-        canvas.itemconfig(item, image=img)
         servo_lock()
+        return img
         # print('locked')
 
     unlocked = contract.events.Unlocked().processReceipt(receipt)
     if unlocked:
         img2 = Image.open('image/display_unlocked_qr.jpeg')
         img2 = ImageTk.PhotoImage(img2)
-        canvas.itemconfig(item, image=img2)
         servo_unlock()
+        return img2
         # print('unlocked')
-    
+
 
 def main():
     thread1 = Thread(target=show_image)
     thread1.start()
-    
+
     w3, contract, contract_address = get_contract_address()
     event_filter = w3.eth.filter({'fromBlock': 'latest', 'address': contract_address})
-    
+
     interval = 2
     while True:
         for event in event_filter.get_new_entries():
             print(event)
-            handle_event(w3, contract, event)
+            img = handle_event(w3, contract, event)
+            canvas.itemconfig(item, image=img)
             time.sleep(interval)
 
 
