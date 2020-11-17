70bbea239601e74be63ddabb775ffb5f8fb49eb5
kentaro-fujita <fujita.kentaro.fk0@is.naist.jp>
2020-11-17 20:05:30 +0900

    画像描画関数の修正

--- controller/watcher/src/watch.py
+++ controller/watcher/src/watch.py
@@ -18,7 +18,7 @@ import json
 
 
 def show_image():
-    global item, canvas
+    global canvas, item, show_img
 
     root = tkinter.Tk()
     root.attributes('-fullscreen', True)
@@ -26,67 +26,80 @@ def show_image():
     root.title('Status')
     root.geometry("1920x1080")
     img = Image.open('image/display_starting.jpeg')
-    img = ImageTk.PhotoImage(img)
+    show_img = ImageTk.PhotoImage(img)
     canvas = tkinter.Canvas(bg="black", width=1920, height=1080)
     canvas.place(x=0, y=0)
-    item = canvas.create_image(0, 0, image=img, anchor=tkinter.NW)
+    item = canvas.create_image(0, 0, image=show_img, anchor=tkinter.NW)
     root.mainloop()
 
 
 def handle_event(w3, contract, event, image_url):
-    receipt = w3.eth.waitForTransactionReceipt(event['transactionHash'])
+    global canvs, item, show_img
+    
+    receipt = w3.eth.getTransactionReceipt(event['transactionHash'])
     locked = contract.events.Locked().processReceipt(receipt)
     if locked:
         try:
             img_read = urllib.request.urlopen(image_url).read()
             img_bin = io.BytesIO(img_read)
             img = Image.open(img_bin)
-            img = ImageTk.PhotoImage(img)
+            show_img = ImageTk.PhotoImage(img)
+            canvas.itemconfig(item, image=show_img)
             servo_lock()
-            return img
-        except:
+            # return img
+        except Exception as e:
             img = Image.open('image/display_locked_qr_demo.jpeg')
-            img = ImageTk.PhotoImage(img)
+            show_img = ImageTk.PhotoImage(img)
+            canvas.itemconfig(item, image=show_img)
             servo_lock()
-            return img
+            print(e)
+            # return img
         # print('locked')
 
     unlocked = contract.events.Unlocked().processReceipt(receipt)
     if unlocked:
-        img2 = Image.open('image/display_unlocked_qr_demo.jpeg')
-        img2 = ImageTk.PhotoImage(img2)
+        img = Image.open('image/display_unlocked_qr_demo.jpeg')
+        show_img = ImageTk.PhotoImage(img)
+        canvas.itemconfig(item, image=show_img)
         servo_unlock()
-        return img2
         # print('unlocked')
 
 
 def get_contract_address():
+    global canvas, item, show_img
     try:
         with open('contracts/lock.json', 'r') as f:
             abi = json.load(f)
     except:
         print('cannot import abi')
 
-    # try:
-    #     with open('contracts/tx_hash.txt', 'r') as f:
-    #         tx_hash = f.read()
-    # except:
-    #     ptint('cannot import tx_hash')
-    
+    # apiからデータを取得
     try:
         url = "http://192.168.10.10:8080/api/products"
         products_get = requests.get(url)
         product_dict = products_get.json()[-1] #最新のレコードを辞書型で取得
-        image_url = product_dict['image'] #imageのURLを取得
-        tx_hash = product_dict['tx_hash'] #tx_hashを取得
-    except:
-        product_dict = {
-            'image':'',
-            'tx_hash':'',
-            }
-        tx_hash = product_dict['tx_hash']
-        image_url = product_dict['image']
+    except Exception as e:
+        print('cannot connect to the dataset api')
     
+    #tx_hashを取得
+    try:
+        tx_hash = product_dict['tx_hash']
+    except:
+        tx_hash = ''
+        print('cannot get tx_hash')
+
+    #imageのURLを取得
+    try:        
+        image_url = product_dict['image']
+        img_read = urllib.request.urlopen(image_url).read()
+        img_bin = io.BytesIO(img_read)
+        img = Image.open(img_bin)
+        show_img = ImageTk.PhotoImage(img)
+        canvas.itemconfig(item, image=show_img)
+    except Exception as e:
+        image_url = ''
+        print('cannot get image_url')
+            
     try:
         infura_url = 'http://geth:8545/'
         w3 = Web3(Web3.HTTPProvider(infura_url))
@@ -111,17 +124,17 @@ def get_contract_address():
 def get_contract():
     interval = 2
     while True:
+        print('next event')
         w3, contract, contract_address, image_url = get_contract_address()
         try:
             event_filter = w3.eth.filter({'fromBlock': 'latest', 'address': contract_address})
             while True:
                 for event in event_filter.get_new_entries():
                     print(event)
-                    img = handle_event(w3, contract, event, image_url)
-                    canvas.itemconfig(item, image=img)
+                    handle_event(w3, contract, event, image_url)
                     time.sleep(interval)
         except Exception as e:
-            print(e)
+            print(f'{e} => search next event')
         time.sleep(interval)
 
 
